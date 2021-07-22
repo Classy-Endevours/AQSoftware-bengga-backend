55311ee0a2b2a9fe89d1d11174973f22f746de4a
Manthan Mehta <>
2021-07-22 13:04:50 +0530

    Verification issue is solved

--- utils/passport.js
+++ utils/passport.js
@@ -4,7 +4,7 @@ const passportJWT = require('passport-jwt');
 const JWTStrategy = passportJWT.Strategy;
 const ExtractJWT = passportJWT.ExtractJwt;
 
-const db = require('../models/index.js');
+const {User} = require('../models/index.js');
 
 passport.use(
 	new JWTStrategy(
@@ -13,15 +13,15 @@ passport.use(
 			secretOrKey: process.env.jwt_token_signing_key,
             issuer: process.env.jwt_token_issuer
 		},
-		(jwtPayload, cb) => 
-            db.sequelize.query('CALL spSelect_UserById (:in_id)', { 
-                replacements: { 
-                    in_id: jwtPayload.id
+		(jwtPayload, cb) => {
+            User.findAll({
+                where : {
+                    id:jwtPayload.id
                 }
             })
             .then((user)=> {
                 cb(null, user.length? user[0]: null)
             })
-            .catch((err) => console.log(err) || cb(err))
+            .catch((err) => console.log(err) || cb(err))}
 	)
 );
